5bedc7add7ad61008179337c6527da375189514d
Howard Yen <hwrdpvt@gmail.com>
2023-03-04 20:05:58 -0500

    Created .env file and replaced varaiable

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
-  client: 'mysql',
+  client: "mysql",
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'rootroot',
-    database: 'dionysus_cabinet_mysql',
-    charset: 'utf8',
-  },
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      charset: process.env.DB_CHAR
+  }
 };
