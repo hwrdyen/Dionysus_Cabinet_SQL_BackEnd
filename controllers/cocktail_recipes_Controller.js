3a42a5da849f8bd4e5ec773ce829e29bf906ff0d
Howard Yen <hwrdpvt@gmail.com>
2023-03-04 19:52:52 -0500

    Created POST function to create new cocktail recipe

--- controllers/cocktail_recipes_Controller.js
+++ controllers/cocktail_recipes_Controller.js
@@ -9,3 +9,19 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Cocktail Recipes: ${err}`)
     );
 };
+
+exports.createCocktailRecipe = (req, res) => {
+  // Validate the request body for required data
+  // if (!req.body.name || !req.body.manager || !req.body.address || !req.body.phone || !req.body.email) {
+  //   return res.status(400).send('Please make sure to provide name, manager, address, phone and email fields in a request');
+  // }
+
+  knex('cocktail_recipes')
+    .insert(req.body)
+    .then((data) => {
+      // For POST requests we need to respond with 201 and the location of the newly created record
+      const newCocktailRecipeURL = `/cocktail_recipes/${data[0]}`;
+      res.status(201).location(newCocktailRecipeURL).send(newCocktailRecipeURL);
+    })
+    .catch((err) => res.status(400).send(`Error creating Cocktail Recipe: ${err}`));
+};
--- routes/cocktail_recipes_Routes.js
+++ routes/cocktail_recipes_Routes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const cocktail_recipes_Controller = require('../controllers/cocktail_recipes_Controller');
 
-router.route('/').get(cocktail_recipes_Controller.index);
+router
+    .route('/')
+    // get all cocktail recipes
+    .get(cocktail_recipes_Controller.index)
+    // create a new cocktail recipe
+    .post(cocktail_recipes_Controller.createCocktailRecipe);
 
 module.exports = router;
