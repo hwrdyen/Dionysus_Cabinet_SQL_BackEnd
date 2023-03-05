8bc3499e817fd8d021e2adbb498319adbfa7738e
Howard Yen <hwrdpvt@gmail.com>
2023-03-04 20:19:31 -0500

    Updated .env and knexfile.js file

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,6 @@ module.exports = {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      charset: process.env.DB_CHAR
+      database: process.env.DB_DATABASE
   }
 };
